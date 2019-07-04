97f27580f5a0b4c1ca60f645e660daa10b9fa402
bybyWind <540804236@qq.com>
2019-07-04 16:41:04 +0800

    新增UIImageView的构造器

---
+++ JokeContent/ZNPlatform/Category/View/UIImageView/UIImageView+ZNCreate.h
@@ -0,0 +1,17 @@
+//
+//  UIImageView+ZNCreate.h
+//  JokeContent
+//
+//  Created by 南木南木 on 2019/7/4.
+//  Copyright © 2019 南木南木. All rights reserved.
+//
+
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImageView (ZNCreate)
+
+@end
+
+NS_ASSUME_NONNULL_END
